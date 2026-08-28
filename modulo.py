46a6c6886583364b484098c32930b2c00edac6f7
NormanBeltran <josenormanbeltran@gmail.com>
2026-08-27 21:57:12 -0300

    Clase_3

---
+++ modulo.py
@@ -0,0 +1,11 @@
+def par(x):
+    return x%2==0
+
+def c_iva(neto):
+    return neto *1.21
+    
+sucursales = ["Buenos Aires", "Nuequen", "La Rioja", "Mendoza"]
+
+nombre_empresa = "Solidaridad SRL"
+
+
